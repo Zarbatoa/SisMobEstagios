2806a9a146d720ac617eed97c685b5648f2393cb
Lucas Zarbato <lpzarbato@gmail.com>
2022-05-31 11:42:23 -0300

    Criado um esboço para a página inicial

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -10,8 +10,80 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Sistema Controle de Estágios'),
+        backgroundColor: Colors.blue[900],
+      ),
+      body: Container(
+        child: SingleChildScrollView(
+          child: Column(
+            children: [
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Container(
+                    padding: const EdgeInsets.only(top: 20),
+                    height: 180,
+                    child: Image.asset('images/logo_sce.png', fit: BoxFit.fitHeight)
+                  )
+                ],
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  imagePadding('images/sobre_logo.png', 'Sobre'),
+                  imagePadding('images/estagiario_logo.png', 'Estagiário'),
+                ],
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  imagePadding('images/curso_logo.png', 'Curso'),
+                  imagePadding('images/empresa_logo.png', 'Empresa')
+                ],
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  imagePadding('images/instituicao_ensino_logo.png', 'Instituição'),
+                  imagePadding('images/estagio_logo.png', 'Estágio')
+                ]
+              )
+            ]
+          )
+        )
+      ),
     );
   }
+
+  Widget imageGestureDetector(String asset, String description, BuildContext context, Widget outraPagina) {
+    return Container(
+      padding: const EdgeInsets.all(25),
+      child: GestureDetector(
+        onTap: () {
+          setState(() {
+            Navigator.push(
+              context,
+              MaterialPageRoute(builder: (context) => outraPagina)
+              );
+          });
+        },
+        child: Image.asset(asset),
+      )
+    );
+  }
+
+  Widget imagePadding(String asset, String description) {
+    return Container(
+      padding: const EdgeInsets.all(30),
+      child: Column(
+        children: [
+          Image.asset(asset),
+          Text(description, style: const TextStyle(fontWeight: FontWeight.bold, fontSize: 18),)
+        ]
+      )
+    );
+  }
+
 }
